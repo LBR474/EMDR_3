be397ea3b6258a808c36f13f7bcedc72db257551
Fake Name <fakename@fake.com>
2024-07-04 16:49:34 +1000

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { templateCompilerOptions } from "@tresjs/core";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/EMDR_1/",
+  base: "/EMDR_3/",
   plugins: [
     vue({
       ...templateCompilerOptions,
